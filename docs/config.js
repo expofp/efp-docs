7bac63b6e0b9e2dcc1078f60da4109ec5fc4e792
Alexander Shagin <alexsh@expofp.com>
2023-04-07 21:34:18 +0300

    EFP-2610: Bootstrap

---
+++ docs/config.js
@@ -0,0 +1,13 @@
+// https://vitepress.dev/guide/getting-started#the-config-file
+// https://github.com/vuejs/vitepress/blob/main/docs/.vitepress/config.ts
+export default {
+    // site-level options
+    title: 'ExpoFP Documentation',
+    description: 'ExpoFP Documentation',
+
+    lastUpdated: true,
+    cleanUrls: true,
+
+    themeConfig: {
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+---
+title: ExpoFP Documentation
+---
+
+# efp-docs
+ExpoFP Documentation
